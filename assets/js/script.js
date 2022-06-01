601de877832458e43c1becd3a06dcdc572521bd3
Samuel <samuelgarenas@gmail.com>
2022-06-01 18:59:18 -0400

    css done and now starting the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 
+//defining all the variables with their required element
 var startButton = document.getElementById(".start_btn button");
 
-var info_box = document.getElementById(".info_box");
-
-var nextButton = document.getElementById('next-btn');
-
-var questionContainerElement = document.getElementById('question-container');
-
-var questionElement = document.getElementById('question');
-
-var answerButtonsElement = document.getElementById('answer-buttons');
-
-var shuffledQuestions, currentQuestionIndex
+var info_box = document.querySelector(".info_box");
+var exit_button = info.box.querySelector(".controls .quit");
+var next_button = document.querySelector(".controls .start");
 
 
-startButton.addEventListener('click', startGame);
 
-nextButton.addEventListener('click', ()=>{
-    currentQuestionIndex++
-    setNextQuestion();
-})
 
+//if start button cliked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); 
+}
+
+exit_btn.onclick =()=>{
+    info_box.classList.remove("activeInfo"); //hiding the rules
+
+}
 
 var questions = [
     {
@@ -70,89 +67,20 @@ var questions = [
 function startGame(){
     console.log('started');
 
-    startButton.classList.add('hide');
-    questionContainerElement.classList.remove('hide');
-
-
-    shuffledQuestions = questions.sort(()=>Math.random()-.5)
-    currentQuestionIndex = 0
-
-    setNextQuestion()
-
 }
 
 function showQuestion(question){
-    questionElement.innerText = question.question
-
-    question.answers.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        
-        if(answer.correct){
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-
-    });
 
 }
 
 function selectAnswer(e){
-    var selectedButton = e.target;
-    var correct = selectedButton.dataset.correct
-    
-    setStatusClass(document.body, correct);
-
-    Array.from(answerButtonsElement.children).forEach(button=>{
-        setStatusClass(button, button.dataset.correct)
-    })
-
-    if (shuffledQuestions.length > currentQuestionIndex + 1){
-        nextButton.classList.remove('hide')
-    }else{
-        startButton.innerText = 'Restart'
-        startButton.classList.remove('hide')
-    }
-    
-}
-
-function setStatusClass(element, correct){
-    ClearStatusClass(element)
-    if (correct){
-        element.classList.add('correct')
-    }else{
-        element.classList.add('wrong')
-    }
-}
-
-function ClearStatusClass(element){
-    element.classList.add('correct')
-    element.classList.add('wrong')
-}
-
-
-
-function resetState(){
-    ClearStatusClass(document.body)
-
-    nextButton.classList.add('hide')
-    while(answerButtonsElement.firstChild){
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
 
 }
 
-function setNextQuestion(){
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
 
-}
 
-function ClearStatusClass(element){
-    element.classList.add('correct')
-    element.classList.add('wrong')
-}
+
+
+
 
 
